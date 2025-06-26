deae0d0ded4ce86ba72e2ec103a896101ae9ae73
mngdinh <minhdc.uni@gmail.com>
2025-06-26 22:49:05 +0700

    yolo setup

---
+++ ScoreLens_FastApi/third_party/yolov5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yolov5',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'opencv-python',
+        'PyYAML',
+        'torch',
+        'torchvision',
+        'tqdm',
+    ],
+)
